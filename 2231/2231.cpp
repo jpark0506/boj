8c1fed9c2c521f88da924e15dfbbdbf8badfc7c2
jpark0506 <peterp1@inha.edu>
2022-03-03 17:18:28 +0900

    D+1 3/3 boj 2231

---
+++ 2231/2231.cpp
@@ -0,0 +1,33 @@
+﻿//20220303
+#include<iostream>
+#include<math.h>
+#include<string>
+//각 자리수 파악을 위해 string으로 바꾸기
+using namespace std;
+int main() {
+	int input;
+	cin >> input;
+	int count = 1;
+	int divsum = 0;
+	while (divsum == 0) {
+		
+		int temp = 0;
+		string str1 = to_string(count);
+		for (int i = 0; i < str1.length(); i++) {
+			temp = temp + str1[i] - '0';
+		}
+		
+		temp = temp + count;
+
+		if (temp == input) {
+			divsum = count;
+			break;
+		}
+		if (count > input) {
+			count = 0;
+			break;
+		}
+		count++;
+	}
+	cout << count;
+}
