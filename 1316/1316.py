23fa431dfd93e38aeaed6ad40c82316e05ef85db
JunhyeokPark(박준혁) <peterp1@inha.edu>
2022-10-08 09:48:00 +0000

    2022-10-08 / 1316 / 단계별로 풀어보기 : 문자열 / Python

---
+++ 1316/1316.py
@@ -0,0 +1,28 @@
+N = int(input())
+# 같을 때, 통과
+# 다를 때 이전 단어 리스트에 있었는지 확인하고 아니면 리스트에 추가
+# 있으면 그룹 단어 실패
+# 아니면 그룹 단어 아님
+cnt = 0
+for i in range(N):
+    M = input()
+    letter_list = [M[0]]
+    flag = True
+    for j in range(len(M)-1):
+        if M[j] != M[j+1]:
+            inner_flag = True
+            
+            for k in letter_list:
+                if M[j+1] == k:
+                    inner_flag = False
+                    break
+            if inner_flag == False:
+                flag = False
+                break
+            else:
+                letter_list.append(M[j+1])
+        else:
+            letter_list
+    if flag:
+        cnt += 1
+print(cnt)
